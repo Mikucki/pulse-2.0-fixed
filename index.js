2a66ca262fc3cafab8c0b059a58a07ded164ba9e
Mikucki <piotr.mikucki@op.pl>
2023-01-08 09:43:50 +0100

    added styles to the main page and made the bot smarter :D

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const puppeteer = require('puppeteer');
 
-async function scrapeData() {
+(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.goto('https://www.nieruchomosci-online.pl/');
 
-  // Select the element you want to scrape, and use the textContent property to get its contents
-  const title = await page.$eval('h1', el => el.textContent);
+  // Type into search box.
+  await page.type('.input-a', 'Gdańsk');
 
-  console.log(title);
+  // Wait for suggest overlay to appear and click "show all results".
+  const allResultsSelector = '.submit-a';
+  await page.waitForSelector(allResultsSelector);
+  await page.click(allResultsSelector);
+
+  // Wait for the results page to load and display the results.
+  console.log('here')
+  const resultsSelector = '.tile-tile';
+  await page.waitForSelector(resultsSelector);
+
+  // Extract the results from the page.
+  const links = await page.evaluate(resultsSelector => {
+    return [...document.querySelectorAll(resultsSelector)].map(anchor => {
+      const title = anchor.textContent.split('|')[0].trim();
+      return `${title} - ${anchor.href}`;
+    });
+  }, resultsSelector);
+
+  // Print all the files.
+  console.log(links.join('\n'));
 
   await browser.close();
-}
-
-scrapeData();
-
+})();
