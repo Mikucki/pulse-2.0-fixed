954f8f3573003ffb2fc712481ab544217f077695
Mikucki <piotr.mikucki@op.pl>
2023-01-12 09:52:45 +0100

    added object creation

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-const puppeteer = require('puppeteer');
-
-function Ofert(make, model, year) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
-}
-
+const puppeteer = require("puppeteer");
+let location = "Gdynia";
+let priceMax = 400000;
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  let localizacion = 'Warszawa'
-  let priceMax = 400000
 
-  await page.goto(`${"https://www.nieruchomosci-online.pl/szukaj.html?3,mieszkanie,sprzedaz,,"} + ${localizacion} + ${':,,,,-'} + ${priceMax}`)
-  
-  
+  await page.goto(
+    `${"https://www.nieruchomosci-online.pl/szukaj.html?3,mieszkanie,sprzedaz,,"} + ${location} + ${":,,,,-"} + ${priceMax}`
+  );
 
-  // Type into search box.
- // await page.type('.input-a', 'Gdynia');
-  
-
-  // Wait for the results page to load and display the results.
-  const resultsSelector = '.tertiary';
-  await page.waitForSelector(resultsSelector);
-
-  // Extract the results from the page.
-  const links = await page.evaluate(resultsSelector => {
-    return [...document.querySelectorAll(resultsSelector)].map(anchor => {
-      const title = anchor.textContent;
-      return `${title} ${'---------------------------------------------------------------'}`;
+  const resultsSelector = ".tertiary";
+  await page.waitForSelector(".tertiary");
+  let offers = await page.evaluate((priceMax, location) => {
+    let elements = document.querySelectorAll(".tertiary");
+    return Array.from(elements, (element) => {
+      return {
+        offer: element.innerText.replace(/\n/g, "----------"),
+        title: "-----",
+        location: 123123123123,
+        city: "Gdynia",
+        priceMax: 123123123,
+      };
     });
-  }, resultsSelector);
-  
+  });
+  console.log(offers);
 
   // Print all the files.
-  console.log(links.join())
   await browser.close();
 })();
